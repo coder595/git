bd1355b5899b59c75e4f20946ad101afc6240640
coder595 <muhammad@github.com>
2024-02-12 17:52:04 +0400

    test commit

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Bash script for viewing some system info.
-
+#------------------------------------------
 cpuinfo=$(lscpu | grep Architecture && lscpu | grep Address && lscpu | grep Thread && lscpu | grep socket && lscpu | grep CPU && lscpu | grep Vendor && lscpu | grep Model && lscpu | grep Virtualization)
 raminfo=$(lsmem)
 osinfo=$(pfetch)
