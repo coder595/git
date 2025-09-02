739865ba0a9d1fb0cec234768a6067fa21fd4a85
ThinkPad <muhammadcnouman@gmail.com>
2025-09-03 02:31:43 +0400

    Updated 03.09.2025

--- dwm/compile-scipt.sh
+++ dwm/compile-scipt.sh
@@ -4,6 +4,7 @@ rm -rf config.h
 sudo make clean install
 sleep 0.5
 rm -rf config.h
+rm -rf *.o *.rej *.orig 
 clear
 date
 figlet Compiled!
--- dwm/config.def.h
+++ dwm/config.def.h
@@ -1,9 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
+/* fonts */
+static const char *fonts[]          = { "FontAwesome:size=16" };
+static const char dmenufont[]       = "FontAwesome:size=14";
 /* appearance */
 static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int gappx     = 10;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+//static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 40;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
@@ -11,28 +15,45 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-#define ICONSIZE 24   /* icon size */
-#define ICONSPACING 5 /* space between icon and title */
-static const char *fonts[]          = { "FontAwesome:size=14", "FontAwesome:size=14" };
-static const char dmenufont[]       = "FontAwesome:size=14";
-static const char normbgcolor[]           = "black";
-static const char normbordercolor[]       = "gray"; //border color
-static const char normfgcolor[]           = "aqua"; //DWM & Slstatus font color
-static const char selfgcolor[]            = "white"; //DWM bar font color
-static const char selbordercolor[]        = "#005577"; /* color dark cyan */
-static const char selbgcolor[]            = "#009933"; //DWM bar color
-static const char *colors[][3] = {
-       /*               fg           bg           border   */
-       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
-       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+//#define ICONSIZE 24   /* icon size */
+#define ICONSIZE 32   /* icon size */
+//#define ICONSPACING 5 /* space between icon and title */
+#define ICONSPACING 15 /* space between icon and title */
+//static const char *fonts[]          = { "FontAwesome:size=14", "FontAwesome:size=14" };
+
+/* Gruvbox Dark Hard Theme */
+static const char normbgcolor[]     = "#282828";  /* background */
+static const char normfgcolor[]     = "#ebdbb2";  /* foreground */
+static const char normbordercolor[] = "#504945";  /* unfocused border */
+
+static const char selbgcolor[]      = "#458588";  /* focused window bg */
+static const char selfgcolor[]      = "#ebdbb2";  /* focused window fg */
+static const char selbordercolor[]  = "#fabd2f";  /* focused border */
+
+/* Extra Gruvbox palette (if you want for tags/urgent) */
+static const char black[]           = "#1d2021";
+static const char red[]             = "#cc241d";
+static const char green[]           = "#98971a";
+static const char yellow[]          = "#d79921";
+static const char blue[]            = "#458588";
+static const char purple[]          = "#b16286";
+static const char aqua[]            = "#689d6a";
+static const char gray[]            = "#a89984";
+static const char orange[]          = "#d65d0e";
+
+static const char *colors[][3]      = {
+	/*               fg            bg            border   */
+	[SchemeNorm] = { normfgcolor,  normbgcolor,  normbordercolor },
+	[SchemeSel]  = { selfgcolor,   selbgcolor,   selbordercolor  },
 };
 
+
 /*screenshot */
-static const char *screenshot[] = {"scrot", "--silent", "/home/muhammad/Pictures/Screenshots/%Y-%m-%d-%T-screenshot.png", NULL};
-static const char *screenshot_window[] = {"scrot", "--select", "--silent", "/home/muhammad/Pictures/Screenshots/%Y-%m-%d-%T-screenshot.png", NULL};
+// static const char *screenshot[] = {"scrot", "--silent", "/home/muhammad/Pictures/Screenshots/%Y-%m-%d-%T-screenshot.png", NULL};
+// static const char *screenshot_window[] = {"scrot", "--select", "--silent", "/home/muhammad/Pictures/Screenshots/%Y-%m-%d-%T-screenshot.png", NULL};
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6" };
+static const char *tags[] = { "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -71,24 +92,43 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, "-hp", "pcmanfm,firefox-esr,chromium,codium,freetube,floorp,syncthingtray,discord,anydesk", NULL };
+
+
+static const char *dmenucmd[] = {
+    "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
+    "-nb", normbgcolor,    /* normal background (#282828) */
+    "-nf", normfgcolor,    /* normal foreground (#ebdbb2) */
+    "-sb", selbgcolor,     /* selected background (#458588) */
+    "-sf", selfgcolor,     /* selected foreground (#ebdbb2) */
+    "-hp", "pcmanfm,firefox-esr,chromium,codium,freetube,floorp,syncthingtray,discord,org.onlyoffice.desktopeditors,anydesk", /* My important Apps */
+    NULL
+};
+
+
+
 static const char *termcmd[]  = { "alacritty", NULL };
 /* Extra Commands */
-static const char *firefoxcmd[]  = { "firefox", NULL };
+static const char *vmmanangercmd[]  = { "virt-manager", NULL };
+static const char *firefoxcmd[]  = { "zen-browser", NULL };
 static const char *slockcmd[]    = { "slock", NULL };
-static const char *chatgptcmd[]  = { "chromium", "--profile-directory=Default",  "--app=https://chat.openai.com", NULL };
-static const char *githubcmd[]  = { "chromium", "--profile-directory=Default",  "--app=https://github.com/", NULL };
-static const char *whatsapp[]  	 = { "chromium", "--profile-directory=Profile 2", "--app=https://web.whatsapp.com", NULL };
+static const char *betterlockscreencmd[]    = { "betterlockscreen", "--lock", NULL };
+static const char *chatgptcmd[]  = { "chromium", "--profile-directory=Profile 2",  "--app=https://chat.openai.com", NULL };
+static const char *githubcmd[]  = { "chromium", "--profile-directory=Profile 2",  "--app=https://github.com/", NULL };
+// static const char *whatsapp[]  	 = { "chromium", "--profile-directory=Profile 4", "--app=https://web.whatsapp.com", NULL };
+static const char *whatsapp[]  = { "flatpak", "run", "com.rtosta.zapzap", NULL };
 static const char *filemanager[]  = { "pcmanfm", NULL };
-//static const char *officecmd[]  = { "flatpak", "run", "org.onlyoffice.desktopeditors", NULL };
+static const char *officecmd[]  = { "flatpak", "run", "org.onlyoffice.desktopeditors", NULL };
 /* volume controls */
-static const char *upvol[]   = { "amixer", "-q", "sset", "Master", "5%+", NULL };
-static const char *downvol[] = { "amixer", "-q", "sset", "Master", "5%-", NULL };
-static const char *mutevol[] = { "amixer", "-q", "sset", "Master", "toggle", NULL };
-static const char *unmutevol[] = { "amixer", "-q", "sset", "Master", "unmute", NULL };
+static const char *volup[]   = { "pamixer", "-i", "5", NULL };
+static const char *voldown[] = { "pamixer", "-d", "5", NULL };
+static const char *volmute[] = { "pamixer", "-t", NULL };
+// static const char *upvol[]   = { "amixer", "-q", "sset", "Master", "5%+", NULL };
+// static const char *downvol[] = { "amixer", "-q", "sset", "Master", "5%-", NULL };
+// static const char *mutevol[] = { "amixer", "-q", "sset", "Master", "toggle", NULL };
+// static const char *unmutevol[] = { "amixer", "-q", "sset", "Master", "unmute", NULL };
 /* brightness controls */
-// static const char *inc_brightness[] = { "xbacklight", "-inc", "10", NULL };
-// static const char *dec_brightness[] = { "xbacklight", "-dec", "10", NULL };
+//static const char *inc_brightness[] = { "xbacklight", "-inc", "10", NULL };
+//static const char *dec_brightness[] = { "xbacklight", "-dec", "10", NULL };
 static const char *brightness_up[]   = { "light", "-A", "10", NULL };
 static const char *brightness_down[] = { "light", "-U", "10", NULL };
 
@@ -98,21 +138,22 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	/* Keybindings for Extra Commands/Apps ------------------------------------- */
 	{ MODKEY,                		XK_q,      spawn,          {.v = firefoxcmd } },
+	{ MODKEY|ShiftMask, 			XK_v,      spawn, 	   	   {.v =  vmmanangercmd} },
 	{ MODKEY, 						XK_l,      spawn, 	   	   {.v = slockcmd } },
+	{ MODKEY|ShiftMask, 			XK_l,      spawn, 	   	   {.v =  betterlockscreencmd} },
 	{ MODKEY, 						XK_g, 	   spawn, 	   	   {.v = chatgptcmd } },
 	{ MODKEY, 						XK_w, 	   spawn, 	   	   {.v = whatsapp } },
 	{ MODKEY, 						XK_e, 	   spawn, 	   	   {.v = filemanager } },
-	{ MODKEY|ShiftMask, 					XK_g, 	   spawn, 	   	   {.v = githubcmd } },
-//	{ MODKEY|ShiftMask,             XK_o,      spawn,          {.v = officecmd } },
-	{ MODKEY,                       XK_F8,     spawn,          {.v = upvol   } },
-	{ MODKEY,                       XK_F7,     spawn,          {.v = downvol } },
-	{ MODKEY,                       XK_F5,     spawn,          {.v = mutevol } },
-	{ MODKEY,                       XK_F6,     spawn,          {.v = unmutevol } },
-	{ MODKEY, 						XK_F2,	   spawn, 	       {.v = brightness_up } },
-    { MODKEY, 						XK_F3,	   spawn, 	   	   {.v = brightness_down } },
+	{ MODKEY|ShiftMask, 			XK_g, 	   spawn, 	   	   {.v = githubcmd } },
+	{ MODKEY|ShiftMask,             XK_o,      spawn,          {.v = officecmd } },
+	{ MODKEY,                       XK_F3,     spawn,          {.v = volup   } },
+	{ MODKEY,                       XK_F2,     spawn,          {.v = voldown } },
+	{ MODKEY,                       XK_F1,     spawn,          {.v = volmute } },
+	{ 0, 						    XK_F6,	   spawn, 	       {.v = brightness_up } },
+    { 0, 						    XK_F5,	   spawn, 	   	   {.v = brightness_down } },
 	/* ------------------------------------------------------------------------- */
-	{0,                             XK_Print,  spawn,	       {.v = screenshot } },
-	{ MODKEY|ShiftMask,             XK_s,	   spawn,	       {.v = screenshot_window } },
+    { 0,                            XK_Print,  spawn,           SHCMD("flameshot gui") },
+    { MODKEY,                       XK_u,      spawn,           SHCMD("ulauncher") },
 	{ MODKEY,                       XK_p,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
